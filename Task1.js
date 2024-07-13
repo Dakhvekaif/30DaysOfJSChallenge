f64e93c4025ec200a05fe0d17c400e4626bf5be7
Dakhve Kaif <kaifdaakve@gmail.com>
2024-07-13 20:55:10 +0530

    Completed Day1 Challenge

---
+++ Task1.js
@@ -0,0 +1,50 @@
+// Task 1
+
+let number = 100;
+console.log(number);
+let aString = "Hello Buddy";
+console.log(aString);
+
+// Task 2
+
+const Job = "Software Engineer"
+console.log(Job);
+
+// Task 3☺
+
+
+let marks = 100
+let Name = "Kaif"
+let isParticipating = true
+let data = {
+    name: "Kaif",
+    Class: "4th year",
+    current_status: "Looking for job",
+}
+let subjects = ["ADvance Artificial intelligence", "Quantum Computing", "Project Management", "Social Medial Analytics"]
+
+console.log(marks)
+console.log(typeof(marks))
+console.log(Name)
+console.log(typeof(Name))
+console.log(isParticipating)
+console.log(typeof(isParticipating))
+console.log(data)
+console.log(typeof(data))
+console.log(subjects)
+console.log(typeof(subjects))
+
+// Task 4
+
+let x = 10
+let y = x
+x = 15
+console.log(y);
+console.log(x);
+
+// Task 5
+
+const userID = "Kaif101"
+// userID = "admin" | TypeError: Assignment to constant variable.
+console.log(userID);
+
