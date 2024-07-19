9b58c57a5bd0c577d12fee81d91b4feff8f0f912
Dakhve Kaif <kaifdaakve@gmail.com>
2024-07-19 18:39:03 +0530

    Completed day 6 challenge

--- day6.js
+++ day6.js
@@ -17,10 +17,31 @@ const mapfunction = newArray.map((x)=>{
 })
 console.log(mapfunction); // task 7 
 
-const numArray = [1,2,3,4,5,6,7,8,9,10]
+const numArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 const filter1 = numArray.filter((num)=>{
     return num % 2 == 0
 })
-console.log(filter1);
+console.log(filter1); // task 8
 
+
+const initialValue = 0
+const reducer1 = numArray.reduce((accumulator,currentValue)=> accumulator + currentValue);
+console.log(reducer1); // task 9
+
+for(i=0;i<=numArray.length-1;i++){
+    // console.log(numArray[i]);
+}                      // task 10
+
+console.log(numArray.forEach((num)=>{
+    // console.log(num);
+}));                   // task 11
+
+const twodArray = [[1,2,8],
+    [8,7,10],
+    [5,7,9]
+]
+console.log(twodArray);  // task 12
+
+console.log(twodArray[0][1]);
+console.log(twodArray[2][2]);  // task 13
