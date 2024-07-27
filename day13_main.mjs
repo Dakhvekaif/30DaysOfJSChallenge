98a7ea2b10359b95bf3ef69329b0b64b3f64ca92
Dakhve Kaif <kaifdaakve@gmail.com>
2024-07-27 21:21:08 +0530

    day13 half done

---
+++ day13_main.mjs
@@ -0,0 +1,18 @@
+import userData1, { sum, person, multiply } from './day13_myModule.mjs';
+import * from 'day13_myModule.mjs' as Obj;
+
+console.log(sum(10,20));    // task 1
+
+console.log(person); // task 2
+
+console.log(multiply(10,10)); // task 3
+console.log(userData1()) // task 4
+
+ 
+
+
+
+
+
+
+
---
+++ day13_myModule.mjs
@@ -0,0 +1,25 @@
+export function sum(num1,num2) {
+    return num1+ num2;
+}
+
+export let person = {
+    name: "kaif",
+    age: 18,
+    School: "rfc"
+}
+
+export function multiply(num1,num2){
+    return num1 * num2
+}
+
+export default function userData1(){
+    return "Hello this is a default function"
+}
+
+const Course_name = "JS Practice";
+const Time = "30 days";
+const completed = false
+
+export * from 'day13_myModule.mjs'
+
+
